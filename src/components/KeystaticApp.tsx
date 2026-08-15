10c4468a4e41f3a58eca8eeb72244984f878ee81
Yudhi <yudhi@192.168.1.11>
2026-08-15 22:51:04 +0700

    feat: embed real-time character counter into keystatic admin page using KeystaticApp component

---
+++ src/components/KeystaticApp.tsx
@@ -0,0 +1,5 @@
+import { makePage } from '@keystatic/astro/ui';
+import config from '../../keystatic.config';
+
+export const KeystaticAdminApp = makePage(config);
+export default KeystaticAdminApp;
